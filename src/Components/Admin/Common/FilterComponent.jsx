972ec4290fb602139970615f84c68b2551c9dcce
payal <payal.varu@empyrealinfotech.com>
2022-06-22 17:22:37 +0530

    add changes

--- src/Components/Admin/Common/FilterComponent.jsx
+++ src/Components/Admin/Common/FilterComponent.jsx
@@ -91,7 +91,7 @@ const CommonMerchant = () => {
                             <li>
                                 <div className="multipleSelection">
                                     <FormControl sx={{ width: '100%' }}>
-                                        <InputLabel id="demo-multiple-checkbox-label"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-bookmark me-1 select-icon"><path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path></svg>
+                                        <InputLabel id="demo-multiple-checkbox-label"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-user-plus me-1 select-icon"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><line x1="20" y1="8" x2="20" y2="14"></line><line x1="23" y1="11" x2="17" y2="11"></line></svg>
                                             Select User</InputLabel>
                                         <Select
                                             labelId="demo-multiple-checkbox-label"
